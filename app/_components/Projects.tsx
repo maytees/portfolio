8d4471857943bd2d68453e4d8e9f70302f81d8a3
maytees <mateespublicprofile@gmail.com>
2024-08-24 09:08:58 -0400

    feat: add all projects

--- app/_components/Projects.tsx
+++ app/_components/Projects.tsx
@@ -11,6 +11,93 @@ import { motion, Variants } from "framer-motion";
 import { Github, Globe2 } from "lucide-react";
 import Link from "next/link";
 
+interface Project {
+  name: string;
+  desc: string;
+  range: string;
+  skills: string[];
+  source?: string;
+  website?: string;
+}
+
+// Array of projects
+const projects: Project[] = [
+  {
+    name: "Epsonia",
+    desc: "Epsonia is a cybersecurity scoring engine based on the Cyber Patriot scoring engine. After working on Epsonia for about a month, I decided to rewrite the project in Rust.",
+    range: "Feb 2023 / Rewritten May 2023",
+    skills: ["Rust", "Typescript", "Cyber Sec", "Linux", "Bash"],
+    source: "https://github.com/epsonia/epsonia-rs",
+  },
+  {
+    name: "Monki",
+    desc: "Implementation of the Monki Language, from the Writing an Interpreter in Go. I wrote this in Rust, not Go. This is by far the project im most proud of.",
+    range: "June 2023",
+    skills: ["Rust", "Lanugage Design"],
+    source: "https://github.com/maytees/monki",
+  },
+  {
+    name: "Arcade",
+    desc: "Survival minigames Paper plugin for Minecraft. Adds twists to survival Minecraft meant to make the game more challenging to beat. I recently converted the project to Kotlin.",
+    range: "Jul 2023 - Aug 2024",
+    skills: ["Java", "Kotlin", "Paper", "Bukkit", "Minecraft", "OOP Based"],
+    source: "https://github.com/maytees/arcade",
+    website: "https://modrinth.com/plugin/arcade",
+  },
+  {
+    name: "SLHS Namecards",
+    desc: "Project made to help out with sorting business cards with laptop distribution for the South Lakes HS tech office. After scanning barcodes, the application return the order to pick up business cards for them to be alphabetical.",
+    range: "Aug 2024",
+    skills: ["Barcodes", "Deno", "CLI"],
+    source: "https://github.com/maytees/slhs-namecards",
+  },
+  {
+    name: "Not Bad",
+    desc: "Not Bad is a virus I created that enables remote shutdown of any computer. It is comprised of three components: a PowerShell script that listens to an API, a desktop app with an interface for remote shutdown, and a server side API. This project was not used maliciously!",
+    range: "May 2023",
+    skills: [
+      "REST",
+      "Deno",
+      "Powershell",
+      "Deno Deploy",
+      "JSON",
+      "Tauri",
+      "NextJS (Pages)",
+    ],
+    source: "https://github.com/maytees/slhs-namecards",
+  },
+  {
+    name: "NextJS Starter Template",
+    desc: "After seeing that all the SaaS starting templates were paid, I decided to create my own, but not really meant for other people, it's mainly for myself.",
+    range: "July 2024",
+    skills: [
+      "NextJS 14",
+      "React",
+      "TailwindCSS",
+      "ShadcnUI",
+      "Prisma",
+      "PostgresDB",
+      "Resend",
+      "AuthJS v5",
+    ],
+    source: "https://github.com/maytees/nextjs-starter-template",
+  },
+  {
+    name: "Cypat Ubuntu Script",
+    desc: "Automation script meant to fix vulnerabilities in the Cyber Patirot Competition. Works for Ubuntu & Debain. (don't use for personal use, this is meant for the Cyber Patriot competition!)",
+    range: "Nov 2022",
+    skills: ["Python", "Bash", "Cyber Sec", "Linux", "Ubuntu"],
+    source: "https://github.com/maytees/cypat-ubuntu-script",
+  },
+  {
+    name: "Wordle TUI",
+    desc: "This project is exactly as it sounds, a Wordle clone in the terminal. The project also includes keybinds which do different things. The project is written in Rust.",
+    range: "Aug 2024",
+    skills: ["Rust", "Ratatui", "Crossterm"],
+    source: "https://github.com/maytees/wordle-tui",
+  },
+];
+
 const cardVariants: Variants = {
   offscreen: {
     y: 300,
@@ -37,112 +124,25 @@ const Projects = () => {
       </p>
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-5 mt-10">
-        <Project
-          desc={`
-            Epsonia is a cybersecurity scoring engine based on the Cyber Patriot scoring engine.
-            After working on Epsonia for about a month, I decided to rewrite the project in Rust.
-          `}
-          name="Epsonia"
-          range="Feb 2023 / Rewritten May 2023"
-          skills={["Rust", "Typescript", "Cyber Sec", "Linux", "Bash"]}
-          source="https://github.com/epsonia/epsonia-rs"
-        />
-        <Project
-          desc={`
-            Implementation of the Monki Language, from the Writing an Interpreter in Go.
-            I wrote this in Rust, not Go. This is by far the project im most proud of.
-          `}
-          name="Monki"
-          range="June 2023"
-          skills={["Rust", "Lanugage Design"]}
-          source="https://github.com/maytees/monki"
-        />
-        <Project
-          desc={`
-          This project is exactly as it sounds, a Wordle clone in the terminal.
-          The project also includes keybinds which do different things. The
-          project is written in Rust.
-          `}
-          name="Wordle TUI"
-          range="Aug 2024"
-          skills={["Rust", "Ratatui", "Crossterm"]}
-          source="https://github.com/maytees/wordle-tui"
-        />
-        <Project
-          desc={`
-            Project made to help out with sorting business cards with laptop
-            distribution for the South Lakes HS tech office. After scanning barcodes, the
-            application return the order to pick up business cards for them to be
-            alphabetical.
-          `}
-          name="SLHS Namecards"
-          range="Aug 2024"
-          skills={["Barcodes", "Deno", "CLI"]}
-          source="https://github.com/maytees/slhs-namecards"
-        />
-        <Project
-          desc={`
-            Survival minigames Paper plugin for Minecraft. Adds twists to
-            survival Minecraft meant to make the game more challenging to beat.
-            I recently converted the project to Kotlin.
-          `}
-          name="Arcade"
-          range="Jul 2023 - Aug 2024"
-          skills={[
-            "Java",
-            "Kotlin",
-            "Paper",
-            "Bukkit",
-            "Minecraft",
-            "OOP Based",
-          ]}
-          source="https://github.com/maytees/arcade"
-          website="https://modrinth.com/plugin/arcade"
-        />
-        <Project
-          desc={`
-            After seeing that all the SaaS starting templates were paid,
-            I decided to create my own, but not really meant for other people,
-            it's mainly for myself.
-          `}
-          name="NextJS Starter Template"
-          range="July 2024"
-          skills={[
-            "NextJS 14",
-            "React",
-            "TailwindCSS",
-            "ShadcnUI",
-            "Prisma",
-            "PostgresDB",
-            "Resend",
-            "AuthJS v5",
-          ]}
-          source="https://github.com/maytees/nextjs-starter-template"
-        />
-        <Project
-          desc={`
-            Automation script meant to fix vulnerabilities in the Cyber Patirot
-            Competition. Works for Ubuntu & Debain. (don't use for personal use, this
-            is meant for the Cyber Patriot competition!)
-          `}
-          name="Cypat Ubuntu Script"
-          range="Nov 2022"
-          skills={["Python", "Bash", "Cyber Sec", "Linux", "Ubuntu"]}
-          source="https://github.com/maytees/cypat-ubuntu-script"
-        />
+        {projects.map(
+          ({ name, desc, range, skills, source, website }: Project, i) => (
+            <Project
+              key={i}
+              name={name}
+              desc={desc}
+              range={range}
+              skills={skills}
+              website={website ?? ""}
+              source={source ?? ""}
+            />
+          )
+        )}
       </div>
     </div>
   );
 };
 
-const Project = (props: {
-  name: string;
-  range: string;
-  desc: string;
-  skills: string[];
-  source?: string; // Link
-  website?: string; // Link
-}) => {
+const Project = (props: Project) => {
   return (
     <motion.div
       initial="offscreen"
